19016a6a6d1badbaf922da7cc78b175055aeafb3
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 03:16:04 +0000

    Snapshot of branch master taken on 2019-04-07T23:16:02.146-04:00 from "https://github.com/UnofficialJuliaMirror/StringAnalysis.jl-b66b7d2f-f536-51df-9f97-4dfb9d27c005"

---
+++ src/tokenizer.jl
@@ -0,0 +1,59 @@
+"""
+    sentence_tokenize([lang,] s)
+
+Splits string `s` into sentences using `WordTokenizers.split_sentences`
+function to perform the tokenization. If a language `lang` is provided,
+it ignores it ;)
+"""
+sentence_tokenize(s::T) where T<:AbstractString =
+    WordTokenizers.split_sentences(s)
+sentence_tokenize(lang::S, s::T) where {S<:Language, T<:AbstractString} =
+    WordTokenizers.split_sentences(s)
+
+
+"""
+    tokenize_slow([lang,] s)
+
+Splits string `s` into tokens on whitespace using `WordTokenizers.tokenize`
+function to perform the tokenization. If a language `lang` is provided,
+it ignores it ;)
+"""
+tokenize_slow(s::T) where T<:AbstractString = WordTokenizers.tokenize(s)
+tokenize_slow(lang::S, s::T) where {S<:Language, T<:AbstractString} = WordTokenizers.tokenize(s)
+
+
+"""
+    tokenize_fast(doc [;splitter])
+
+Function that quickly tokenizes `doc` based on the splitting
+pattern specified by `splitter::RegEx`.
+Supported types for `doc` are: `AbstractString`, `Vector{AbstractString}`,
+`StringDocument` and `NGramDocument`.
+"""
+tokenize_fast(doc::Vector{S}; splitter::Regex=DEFAULT_TOKENIZATION_REGEX
+             ) where S<:AbstractString =
+    return vcat((tokenize_fast(words) for words in doc)...)
+
+tokenize_fast(doc::S; splitter::Regex=DEFAULT_TOKENIZATION_REGEX
+             ) where S<:AbstractString =
+    strip.(split(doc, splitter, keepempty=false))
+
+tokenize_fast(doc::NGramDocument; splitter::Regex=DEFAULT_TOKENIZATION_REGEX) =
+    unique!(tokenize_fast(collect(keys(doc.ngrams))))
+
+tokenize_fast(doc::StringDocument; splitter::Regex=DEFAULT_TOKENIZATION_REGEX) =
+    tokenize_fast(doc.text, splitter=splitter)
+
+"""
+    tokenize(s [;method])
+
+Tokenizes based on either the `tokenize_slow` or `tokenize_fast`
+functions.
+"""
+function tokenize(doc; method::Symbol=DEFAULT_TOKENIZER)
+    if method == :slow
+        return tokenize_slow(doc)
+    else
+        return tokenize_fast(doc)
+    end
+end
